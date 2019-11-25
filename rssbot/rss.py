3c707fd66b1fad8f2c99885054fa646ffe96d877
Bart Thate <bthate@dds.nl>
2019-11-26 00:06:18 +0100

    fix rssbot

--- rssbot/rss.py
+++ rssbot/rss.py
@@ -1,12 +1,3 @@
-"""
-     rss feed fetcher.
-
-     to fetch all feeds from the commandline use:
-
-         ./bin/ob -m rss fetch
-
-"""
-
 __version__ = 1
 
 import datetime
@@ -37,13 +28,10 @@ def __dir__():
     return ("Cfg", "Feed", "Fetcher", "Rss", "Seen", "delete" ,"display", "feed", "fetch", "init", "rss")
 
 def init():
-    """ rss initialisation. """
     fetcher.start()
     return fetcher
 
 class Cfg(Cfg):
-
-    """ RSS fetcher configuration. """
 
     def __init__(self):
         super().__init__()
@@ -52,11 +40,9 @@ class Cfg(Cfg):
 
 class Feed(Default):
 
-    """ feed entry. """
+    pass
 
 class Rss(Persist):
-
-    """ rss entry to store feed url. """
 
     def __init__(self):
         super().__init__()
@@ -64,15 +50,11 @@ class Rss(Persist):
 
 class Seen(Persist):
 
-    """ stores seen feed urls. """
-
     def __init__(self):
         super().__init__()
         self.urls = []
 
 class Fetcher(Persist):
-
-    """ RSS feed fetcher. """
 
     def __init__(self):
         super().__init__()
@@ -81,7 +63,6 @@ class Fetcher(Persist):
         self._thrs = []
 
     def display(self, o):
-        """ return displayable string of a feed item. """
         result = ""
         try:
             dl = o.display_list
@@ -100,7 +81,6 @@ class Fetcher(Persist):
         return result[:-2].rstrip()
 
     def fetch(self, obj):
-        """ fetch a single feed. """
         counter = 0
         objs = []
         if not obj.rss:
@@ -133,22 +113,19 @@ class Fetcher(Persist):
         return counter
 
     def join(self):
-        """ join a running threads. """
         for thr in self._thrs:
             thr.join()
 
     def run(self):
-        """ run a poll on all registered feeds. """
         res = []
         thrs = []
-        for o in k.db.all("obot.rss.Rss"):
+        for o in k.db.all("rssbot.rss.Rss"):
             thrs.append(k.launch(self.fetch, o))
         for thr in thrs:
             res.append(thr.join())
         return res
 
     def start(self, repeat=True):
-        """ start rss fetcher. """
         last(self.cfg)
         last(self.seen)
         if repeat:
@@ -157,13 +134,11 @@ class Fetcher(Persist):
             return repeater
 
     def stop(self):
-        """ stop rss poller. """
         self.seen.save()
 
 fetcher = Fetcher()
 
 def get_feed(url):
-    """ return entries of a RSS feed. """
     result = ""
     if k.cfg.debug:
         return [Object(), Object()]
@@ -179,55 +154,51 @@ def get_feed(url):
                 yield entry
 
 def file_time(timestamp):
-    """ convert timestamp to filename. """
     return str(datetime.datetime.fromtimestamp(timestamp)).replace(" ", os.sep) + "." + str(random.randint(111111, 999999))
 
 def delete(event):
-    """ delete matching rss objects. """
     if not event.args:
         event.reply("delete <match>")
         return
     selector = {"rss": event.args[0]}
     nr = 0
-    for rss in k.db.find("obot.rss.Rss", selector):
+    for rss in k.db.find("rssbot.rss.Rss", selector):
         nr += 1
         rss._deleted = True
         rss.save()
     event.reply("ok %s" % nr)
 
 def display(event):
-    """ use to add a rss feed or get a overview of registered rss feeds. """
     if len(event.args) < 2:
         event.reply("display <feed> key1,key2,etc.")
         return
     nr = 0
     setter = {"display_list": event.args[1]}
-    for o in k.db.find("obot.rss.Rss", {"rss": event.args[0]}):
+    for o in k.db.find("rssbot.rss.Rss", {"rss": event.args[0]}):
         nr += 1
         ob.edit(o, setter)
         o.save()
     event.reply("ok %s" % nr)
 
 def feed(event):
-    """ use to add a rss feed or get a overview of registered rss feeds. """
     match = ""
     if event.args:
         match = event.args[0]
     nr = 0
     diff = time.time() - to_time(day())
-    res = list(k.db.find("obot.rss.Feed", {"link": match}, delta=-diff))
+    res = list(k.db.find("rssbot.rss.Feed", {"link": match}, delta=-diff))
     for o in res:
         if match:
             event.reply("%s %s - %s - %s - %s" % (nr, o.title, o.summary, o.updated or o.published or "nodate", o.link))
         nr += 1
     if nr:
         return
-    res = list(k.db.find("obot.rss.Feed", {"title": match}, delta=-diff))
+    res = list(k.db.find("rssbot.rss.Feed", {"title": match}, delta=-diff))
     for o in res:
         if match:
             event.reply("%s %s - %s - %s" % (nr, o.title, o.summary, o.link))
         nr += 1
-    res = list(k.db.find("obot.rss.Feed", {"summary": match}, delta=-diff))
+    res = list(k.db.find("rssbot.rss.Feed", {"summary": match}, delta=-diff))
     for o in res:
         if match:
             event.reply("%s %s - %s - %s" % (nr, o.title, o.summary, o.link))
@@ -236,15 +207,13 @@ def feed(event):
         event.reply("no results found")
  
 def fetch(event):
-    """ fetch registered feeds. """
     res = fetcher.run()
     event.reply("fetched %s" % ",".join([str(x) for x in res]))
 
 def rss(event):
-    """ use to add a rss feed or get a overview of registered rss feeds. """
     if not event.rest or "http" not in event.rest:
         nr = 0
-        res = list(k.db.find("obot.rss.Rss", {"rss": ""}))
+        res = list(k.db.find("rssbot.rss.Rss", {"rss": ""}))
         if res:
             for o in res:
                 event.reply("%s %s" % (nr, o.rss))
