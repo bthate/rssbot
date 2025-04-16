a46a53c1f8ac0a4b61e4507dec756085a414de65
Bart Thate <nixtniet@gmail.com>
2025-04-16 06:59:54 +0200

    remove tests

--- rssbot/modules/tbl.py
+++ rssbot/modules/tbl.py
@@ -15,7 +15,8 @@ NAMES = {
     "rem": "rss",
     "res": "rss",
     "rss": "rss",
-    "syn": "rss"
+    "syn": "rss",
+    "ver": "ver"
 }
 
 
@@ -23,4 +24,5 @@ MD5 = {
     "irc": "bd26b077402c7791049ebf5d97a340ef",
     "rss": "25aade0b6f4b56ef9aa0dbc7536bc790",
     "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
 }
